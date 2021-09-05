9a6dd3b1ecbc37dca6701fc227da23e8dabda265
camouflagedName <mazeltii@gmail.com>
2021-09-05 17:01:35 -0400

    updated apiRoot to match new app

--- app/javascript/packs/apiRoot.js
+++ app/javascript/packs/apiRoot.js
@@ -1,4 +1,4 @@
 const devURL = '';
-const prodURL = 'https://murmuring-ridge-43281.herokuapp.com/';
+const prodURL = 'https://uncover-edu.herokuapp.com';
 
 export const API_ROOT = process.env.NODE_ENV === 'development' ? devURL : prodURL;
