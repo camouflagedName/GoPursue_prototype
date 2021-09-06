fd444e5f0c09426f62db7d5f04fb8dd88dd8091e
camouflagedName <mazeltii@gmail.com>
2021-09-06 00:54:52 -0400

    bootstrap icon fix, hopefully

--- app/javascript/components/careerCard.jsx
+++ app/javascript/components/careerCard.jsx
@@ -77,48 +77,40 @@ export default class careerCard extends React.Component {
                         </p>
                     </div>
                     <div className="card-body">
-                        <p className="fw-bold">Education:</p><p> {this.state.careers.education}</p>
-                        <p className="fw-bold">Average Pay:</p><p> {this.state.careers.pay}</p>
-                        <p className="fw-bold">Work environment:</p><p> {this.state.careers.environment}</p>
-                        <p className="fw-bold">Quick Fact:</p><p> {this.state.careers.description}</p>
-                        <div className="col-6 mx-auto">
+                        <p className="fw-bold mb-0">Education:</p>
+                        <p className="mt-0">{this.state.careers.education}</p>
+                        <p className="fw-bold mb-0">Average Pay:</p>
+                        <p>{this.state.careers.pay}</p>
+                        <p className="fw-bold mb-0">Work environment:</p>
+                        <p>{this.state.careers.environment}</p>
+                        <p className="fw-bold mb-0">Quick Fact:</p>
+                        <p>{this.state.careers.description}</p>
+                        <div className="d-flex col-6 mx-auto justify-content-center">
                             <button onClick={this.changeCareer} className="btn btn-primary btn-lg">Surprise Me!</button>
                         </div>
                     </div>
                         <div className="card-footer bg-transparent">
-                            <ul className="nav nav-pills">
-                                <div className="col-3">
-                                    <li className="nav-item col-8 mx-auto">
-                                    <a href="#" className="nav-link  py-3"  title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-house" viewBox="0 0 16 16">
-                                            <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
-                                            <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
-                                        </svg>
-                                    </a>
-                                    </li>
-                                </div>
-                                <div className="col-3">
-                                    <li className="nav-item col-8 mx-auto">
-                                        <a href="#" className="nav-link active link-dark py-3" aria-current="page" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-compass" viewBox="0 0 16 16">
-                                                <path d="M8 16.016a7.5 7.5 0 0 0 1.962-14.74A1 1 0 0 0 9 0H7a1 1 0 0 0-.962 1.276A7.5 7.5 0 0 0 8 16.016zm6.5-7.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
-                                                <path d="m6.94 7.44 4.95-2.83-2.83 4.95-4.949 2.83 2.828-4.95z"/>
+                            <ul className="nav nav-pills rounded-pill nav-justified">
+                                    <li className="nav-item">
+                                        <a href="#" className="nav-link link-dark py-3"  title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-house" viewBox="0 0 16 16">
+                                                <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                                                <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
                                             </svg>
                                         </a>
+                                    </li>       
+                                    <li className="nav-item">
+                                        <div className="col-6 offset-3"> 
+                                            <a href="#" className="nav-link active link-dark py-3" aria-current="page" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-compass" viewBox="0 0 16 16">
+                                                    <path d="M8 16.016a7.5 7.5 0 0 0 1.962-14.74A1 1 0 0 0 9 0H7a1 1 0 0 0-.962 1.276A7.5 7.5 0 0 0 8 16.016zm6.5-7.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+                                                    <path d="m6.94 7.44 4.95-2.83-2.83 4.95-4.949 2.83 2.828-4.95z"/>
+                                                </svg>
+                                            </a>
+                                        </div>
+
                                     </li>
-                                </div>
-                                <div className="col-3">
-                                    <li className="nav-item col-8 mx-auto">
-                                        <a href="#" className="nav-link link-dark py-3" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-chat-text" viewBox="0 0 16 16">
-                                                <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
-                                                <path d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8zm0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z"/>
-                                            </svg>
-                                        </a>
-                                    </li>
-                                </div>
-                                <div className="col-3">
-                                    <li className="nav-item col-8 mx-auto">
+                                    <li className="nav-item">
                                         <a href="#" className="nav-link link-dark py-3" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
                                                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
@@ -126,7 +118,7 @@ export default class careerCard extends React.Component {
                                             </svg>
                                         </a>
                                     </li>
-                                </div>
+                                
                             </ul>
                         </div>
                     </div>
